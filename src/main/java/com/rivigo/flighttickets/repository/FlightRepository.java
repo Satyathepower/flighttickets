8e4d16d8af57325cf6d6211124212792f4395922
satyathepower <satendracisf1989@gmail.com>
2020-06-07 18:44:00 +0530

    d

--- src/main/java/com/rivigo/flighttickets/repository/FlightRepository.java
+++ src/main/java/com/rivigo/flighttickets/repository/FlightRepository.java
@@ -2,7 +2,9 @@ package com.rivigo.flighttickets.repository;
 
 import com.rivigo.flighttickets.entity.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlightRepository extends JpaRepository<Flight,Long> {
 
     public Flight findByFlightNumber(String flightNumber);
